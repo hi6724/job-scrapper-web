2542bf259d35663af6d48b4c38002c110252ad02
hi6724 <hi6724@gmail.com>
2022-01-22 15:31:49 +0900

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div style={{ display: "flex", height: "250px", justifyContent: "center", alignItems: "center" }}>
+        <h1 style={{ fontFamily: "BM-Pro", fontSize: "30px" }}>깃 연습</h1>
+      </div>
+    </>
+  );
+}
